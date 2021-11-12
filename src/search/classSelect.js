76714d9919f210590e9fc5209bfbfb8a9ee9887a
deokisys <sjb378@me.com>
2021-11-12 17:18:14 +0900

    modify 이벤트 활성화 초기화 수행
    직업 변경시 각인 선택 정상작동
    직업변경시 옵션의 각인선택 초기화 진행

--- src/search/classSelect.js
+++ src/search/classSelect.js
@@ -1,4 +1,6 @@
+import { selectActivateEvent } from "../activateSelect"
 import { selectElement } from "./selectElement"
+
 /**
  * 직업 선택 이벤트 함수
  * 선택시 현재 select들을 전부 수정
@@ -14,6 +16,8 @@ export function classSelectEvent(){
 
         mainSelect.forEach(ele=>{
             ele.innerHTML = selectElement("mainSelect")
+            selectActivateEvent(ele.parentNode,"INPUT")//이벤트 부여
+            radioDisable(ele.nextElementSibling)//라디오초기화
         })
 
 
@@ -24,12 +28,13 @@ export function classSelectEvent(){
         optionCheck.checked = false
         //선택창 재설정
         optionSelect.forEach(ele=>{
-            ele.innerHTML = selectElement("optionGakGakin",true)
+            ele.innerHTML = selectElement("optionGakGakin",true)//이벤트가 없음
+            selectActivateEvent(ele.parentNode,"INPUT")//이벤트 부여
+            radioDisable(ele.nextElementSibling)//라디오초기화
         })
         //수정
         
     })
-    
 }
 /**
  * 어떤 직업이 선택되었는지 확인
@@ -38,4 +43,17 @@ export function classSelectEvent(){
 export function classSelectValue(){
     let classSelectForm = document.querySelector(".job .select_form select")
     return classSelectForm.options[classSelectForm.selectedIndex].value
+}
+
+/**
+ * 해당 라디오박스 disable, checkfalse로
+ * @param {*} target 라디오박스를 가지고있는 div
+ */
+function radioDisable(target){
+    target.childNodes.forEach(ele=>{
+        if(ele.tagName==="INPUT"){
+            ele.checked = false//체크해제
+            ele.disabled = true//disable로
+        }
+    })
 }
