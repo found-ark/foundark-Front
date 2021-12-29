78f02f50d02b2c2e8567e201e21c611c6c3680dd
deokisys <sjb378@me.com>
2021-12-29 15:38:15 +0900

    refactor 결과창 재 수정
    결과 출력 부분 기획 처럼 재수정
    색상을 회색,검정,하양으로 수정
    그에 맞게 css도 재수정

--- src/result/resultElement.js
+++ src/result/resultElement.js
@@ -1,28 +1,25 @@
-import {AkElement} from "./akElement";
+import {AccElement} from "./accElement";
 import {SummaryElement} from "./summaryElement"
 
 
 export function ResultElement(data) {  
     let resultElement = document.createElement("div");
     resultElement.classList.add("result_element")
-    
-    let resultAkList = `<div class="result_acc_list">`
-    //악세서리 정보
-    // console.log(data["neck"])
-    Object.keys(data).forEach((ele)=>{
-        // if(data[ele]["name"])
+
+    resultElement.appendChild(SummaryElement(data))
+    let whiteList = [1,0,0,1,1,0,0,1]
+    Object.keys(data).forEach((ele,i)=>{
         if(data[ele]["name"]!==undefined){
-            resultAkList+=AkElement(data[ele])
+            let accEle = AccElement(data[ele])
+            if(whiteList[i]===1){
+                accEle.classList.add("white")
+            }
+            resultElement.appendChild(accEle)
         }
     })
-    resultAkList+="</div>"
 
     //요약
     
-    resultElement.innerHTML = `
-        ${resultAkList}
-        ${SummaryElement(data)}
-    `
     return resultElement
 
 }
--- src/result/summaryElement.js
+++ src/result/summaryElement.js
@@ -1,4 +1,4 @@
-import {getPeon,Abbreviation} from "../util"
+import {getPeon,Abbreviation,isAcc} from "../util"
 
 function sum(target,input){
     Object.keys(input).forEach(ele=>{
@@ -13,20 +13,9 @@ function abilText(input){
     // key value ... 로 출력
 
     Object.keys(input).forEach(key=>{
-        result+=`${key}${input[key]} `
+        result+=`<span>${key} :${input[key]}</span>`
     })
 
-    return result
-}
-
-function engText(input){
-    let result = ""
-    // key value ... 로 출력
-    Object.keys(input).sort((a,b)=>{
-        return input[b]-input[a]
-    }).forEach(key=>{
-        result+=`${Abbreviation[key]}${parseInt(input[key]/5)} `
-    })
     return result
 }
 
@@ -50,35 +39,37 @@ function penText(input){
 }   
 
 export function SummaryElement(data) {  
-    let $smElement = ""
-
+    let smElement = document.createElement("div")
+    smElement.className = "result_summary"
     {/* <b>요약: 요금 40000G 페온 100 특화1500 치명500</b>
     각성3 기습3 정흡3 돌대3 원한3 광기3 <span>감소 없음</span> */}
     let money = 0
     let peon = 0
+    let quality = 0
 
     let abil = {}//ability
-    let eng = {}//engrave
     let pen = {}//penalty
     Object.keys(data).forEach((ele)=>{
         if(data[ele]["name"]!==undefined){
             money+=data[ele]["price"]
             peon+= getPeon(data[ele]["name"])
+            if(isAcc(data[ele]["name"])){
+                quality+=data[ele]["quality"]
+            }
             //특성
             sum(abil,data[ele]["ability"])
-            //engrave(각인)
-            sum(eng,data[ele]["engrave"])
             //penalty(패널티)
             sum(pen,data[ele]["penalty"])
         }
     })
-    $smElement = `
-    <div class="result_summary">
-        <span>
-            <b>요약: 요금 ${money}G 페온 ${peon} ${abilText(abil)}</b>
-            ${engText(eng)}<span style="color:red">${penText(pen)}</span>
-        </span>
+
+    smElement.innerHTML = `
+        <span>${money}G</span>
+        <span>${peon}페온</span>
+        <span style="color:red">${penText(pen)}</span>
+        ${abilText(abil)}
+        <span>${quality/5}</span>
     `
-    return $smElement
+    return smElement
 }
   
