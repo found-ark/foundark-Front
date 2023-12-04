7c9021378ddac6ba53bd139908fbc4165f542124
deokisys <sjb378@me.com>
2023-12-04 16:39:06 +0900

    [feat] webpack수정, 버전 업그레이드

--- webpack.config.js
+++ webpack.config.js
@@ -1,92 +1,72 @@
-const path = require('path');
-const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
-
-const dotenv = require("dotenv");
-dotenv.config({//env가져오기
-    path: path.resolve(//경로찾기
-        process.cwd(),//현재경로 + 
-        process.env.NODE_ENV == "production" ? "env/.production.env" : "env/.development.env"//실제는 .env, 개발은 .env.dev사용
-    ),
-    });
-console.log(process.env.IP)
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
-    // enntry file
-    // - sass가 존재할경우 entry: ['@babel/polyfill', './main.js', './main.scss'],
-    entry: ['@babel/polyfill', './src/main.js', './sass/main.scss'],
-    // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js',
-        publicPath: process.env.IP
+  // enntry file
+  // - sass가 존재할경우 entry: ['@babel/polyfill', './main.js', './main.scss'],
+  entry: ["@babel/polyfill", "./src/main.js", "./sass/main.scss"],
+  // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
+  },
+  //server 옵션
+  devServer: {
+    //contentBase에서 변경됨
+    static: {
+      directory: "dist",
     },
-    devServer: {
-        contentBase: "dist",
-        overlay: true,
-        // 웹팩의 상태값에 색상을 부여한다.
-        stats: {
-          colors: true
+    // hot 프로퍼티를 true로 설정!
+    hot: true,
+  },
+  plugins: [
+    // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
+    new MiniCssExtractPlugin({ filename: "css/style.css" }),
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new FaviconsWebpackPlugin({
+      logo: "./public/favicon.ico",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [path.resolve(__dirname, "src/js")],
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
         },
-        // hot 프로퍼티를 true로 설정!
-        hot: true
       },
-    plugins: [
-        // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
-        new MiniCssExtractPlugin({ filename: 'css/style.css' }),
-        new webpack.DefinePlugin({
-            'process.env.IP': JSON.stringify(process.env.IP), // env에서 읽은 ip를 저장
-            'process.env.API_IP': JSON.stringify(process.env.API_IP), //
-            'process.env.PORT': JSON.stringify(process.env.PORT),
-            'process.env.DUMMY': JSON.stringify(process.env.DUMMY),
-        }),
-        new HTMLWebpackPlugin({
-            template: "./views/index.html"
-          }),
-        new FaviconsWebpackPlugin({
-            logo: './public/favicon.ico',
-        }),
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader", // compiles Sass to CSS, using Node Sass by default
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        loader: "file-loader",
+        options: {
+          publicPath: "",
+          // name: 'images/[name].[ext]',
+          name: "[name].[ext]?[hash]",
+        },
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: [
-                    path.resolve(__dirname, 'src/js')
-                ],
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-proposal-class-properties']
-                    }
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [ 
-                    MiniCssExtractPlugin.loader,            
-                    "css-loader", // translates CSS into CommonJS
-                    "sass-loader", // compiles Sass to CSS, using Node Sass by default
-                ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|webp)$/i,
-                loader: 'file-loader',
-                options:{
-                    publicPath: '',
-                    // name: 'images/[name].[ext]',
-                    name: '[name].[ext]?[hash]'
-                }
-            },              
-        ]
-    },
-    devtool: 'source-map',
-    // https://webpack.js.org/concepts/mode/#mode-development
-    mode: 'development'
+  },
+  devtool: "source-map",
+  // https://webpack.js.org/concepts/mode/#mode-development
 };
