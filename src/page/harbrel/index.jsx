ad38aaed94f3f21ac254b046eb676c331f6d8c51
deokisys <sjb378@me.com>
2023-12-12 22:07:55 +0900

    [refactor] 리액트 컴포넌트 이름 교체

--- src/page/harbrel/index.jsx
+++ src/page/harbrel/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Harbcontent from "./harbcontentR";
+import Harbcontent from "./harbcontent";
 import { initHarbImg } from "../imageloader";
 
 function Harbrel() {
