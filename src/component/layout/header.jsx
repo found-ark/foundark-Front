8a54fcd5e8e600c0253596482ac57b7d9a731e85
deokisys <sjb378@me.com>
2023-12-18 00:24:11 +0900

    [refactor] 제목부분도 dom수정에서  state로 변경

--- src/component/layout/header.jsx
+++ src/component/layout/header.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import harbTitle from "../../assets/title2.svg"
+import ellaTitle from "../../assets/ellatitle.svg"
+
 function Header() {
-  return <div className="header"></div>;
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const [src,setSrc]= useState("");
+
+  useEffect(()=>{
+    if(currentPath==="/harbrel"){
+      setSrc(harbTitle);
+    }else if(currentPath==="/ella"){
+      setSrc(ellaTitle);
+    }
+  },[currentPath])
+
+  return <div className="header">
+    <img src={src} alt={currentPath}/>
+  </div>;
 }
 
 export default Header;
