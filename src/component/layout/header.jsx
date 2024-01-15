c3d229594a7fdee8bfb3bf99d40d6a8e0b812f28
deokisys <sjb378@me.com>
2024-01-15 13:50:57 +0900

    [fix] 초기페이지 이미지 로딩 수정

--- src/component/layout/header.jsx
+++ src/component/layout/header.jsx
@@ -1,28 +1,30 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import harbTitle from "../../assets/title2.svg"
-import ellaTitle from "../../assets/ellatitle.svg"
-import choTitle from "../../assets/chotitle.svg"
+import harbTitle from "../../assets/title2.svg";
+import ellaTitle from "../../assets/ellatitle.svg";
+import choTitle from "../../assets/chotitle.svg";
 
 function Header() {
   const location = useLocation();
   const currentPath = location.pathname;
 
-  const [src,setSrc]= useState("");
+  const [src, setSrc] = useState("");
 
-  useEffect(()=>{
-    if(currentPath==="/harbrel"){
+  useEffect(() => {
+    if (currentPath === "/" || currentPath === "/harbrel") {
       setSrc(harbTitle);
-    }else if(currentPath==="/ella"){
+    } else if (currentPath === "/ella") {
       setSrc(ellaTitle);
-    }else if(currentPath==="/cho"){
+    } else if (currentPath === "/cho") {
       setSrc(choTitle);
     }
-  },[currentPath])
+  }, [currentPath]);
 
-  return <div className="header">
-    <img src={src} alt={currentPath}/>
-  </div>;
+  return (
+    <div className="header">
+      <img src={src} alt={currentPath} />
+    </div>
+  );
 }
 
 export default Header;
